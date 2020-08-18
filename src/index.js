a9850cce5ef5085e02e627191f88119f06fdecbb
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-18 21:58:45 +0300

    updated: index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 'use strict';
 
 /** imports */
+import $ from './tools/get-by-id';
+import {
+  setBodyOverflowToEmpty,
+  setBodyOverflowToHidden,
+} from './tools/body-overflow';
+
+// controllers
 import './controllers/scroll';
 import './controllers/carousel';
 import './controllers/parallax';
@@ -10,3 +17,17 @@ import './controllers/form';
 import './styles/main.scss';
 import './assets/fonts';
 import './assets/images';
+
+/** init */
+setBodyOverflowToHidden();
+
+window.onload = () => {
+  const root = $('root');
+  const loader = $('loader');
+  root.classList.add('is-active');
+  loader.classList.add('is-hide');
+  setBodyOverflowToEmpty();
+  setTimeout(() => {
+    loader.remove();
+  }, 1000);
+}

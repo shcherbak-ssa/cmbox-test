40b8115432227f2252dba3c9952e5233682a377e
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-08-18 14:23:46 +0300

    created: input controller

---
+++ src/controllers/input.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/** Input */
+class Input {
+  constructor(input) {
+    this._input = input;
+    this._inputType = this._input.dataset.inputType;
+    this._inputClasseList = this._input.classList;
+    this._inputField = this._input.querySelector('.input-field');
+    this._inputError = this._input.querySelector('.input-error');
+  }
+
+  /** static methods */
+  static create(input) {
+    return new Input(input);
+  }
+
+  /** public methods */
+  setEvents() {
+    this._input.addEventListener('click', this._clickHandler);
+    this._inputField.addEventListener('input', this._inputHandler);
+    this._inputField.addEventListener('blur', this._blurHandler);
+  }
+  getValue() {
+    return this._inputField.value;
+  }
+  setError(error) {
+    this._inputError.innerHTML = error;
+    this._setClassname('is-error');
+  }
+
+  /** private methods */
+  _clickHandler() {
+    if (!this._hasValue()) this._setClassname('is-filled');
+    this._addFocus();
+  }
+  _inputHandler({target: {value}}) {
+
+  }
+  _blurHandler() {
+    if (this._hasValue()) return;
+    this._removeClassname('is-filled');
+  }
+
+  _hasValue() {
+    return !!this.getValue();
+  }
+  _addFocus() {
+    this._inputField.focus();
+  }
+  _setClassname(classname) {
+    this._inputClasseList.add(classname);
+  }
+  _removeClassname(classname) {
+    this._inputClasseList.remove(classname);
+  }
+}
+
+/** export */
+export default Input
